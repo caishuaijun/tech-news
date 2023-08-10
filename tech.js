9c3b06df0da27fe2fc0400408361253517489e2a
zhangbao <zhangbao90s@qq.com>
2023-08-10 13:24:55 +0800

    feat: auto generate data
    via github action's schedule task

--- tech.js
+++ tech.js
@@ -3,13 +3,26 @@ const { JSDOM } = require('jsdom');
 const { getWeekly } = require('./weekly');
 const fs = require('node:fs/promises');
 
-;(async function () {
+; (async function () {
   const JsUrl = 'https://javascriptweekly.com/issues/latest';
   const FeUrl = 'https://frontendfoc.us/latest';
-  const [response1, response2] = await Promise.all([ axios.get(FeUrl), axios.get(JsUrl) ])
+  const [response1, response2] = await Promise.all([axios.get(FeUrl), axios.get(JsUrl)])
   const { window: { document: feDoc } } = new JSDOM(response1.data, { FeUrl })
   const { window: { document: jsDoc } } = new JSDOM(response2.data, { JsUrl })
-  const weeklyFeed = getWeekly(jsDoc, feDoc)
+
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+
+  let docs = []
+
+  if (dayOfWeek === 4) {
+    docs = [feDoc]
+  } else if (dayOfWeek === 5) {
+    docs = [jsDoc, feDoc]
+  }
+
+  const weeklyFeed = getWeekly(...docs)
+
 
   await fs.rm('./dist', { recursive: true });
   console.log(`successfully deleted ./dist`);
